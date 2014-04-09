37415da4de87e17339366f144f6bfa461623cd6e
apollolms-stephan <stephanpieterse@apollolms.co.za>
2014-04-09 07:27:53 +0200

    more fancyness and less suckage

--- php/processCard.php
+++ php/processCard.php
@@ -5,31 +5,29 @@
 	$p_age = $_POST['age'];
 	$p_marital = $_POST['marital_status'];
 	$p_lang = $_POST['lang'];
+	$p_occupation = $_POST['occupation']
 	
 	$p_reqtype = $_POST['request'];
 	
-	switch($p_reqtype){
-		case 'req_acceptjesus':
+	$p_reqtype_text = "";
+		if(isset($_POST'req_acceptjesus')){
 			$p_reqtype_text = "I want to accept Jesus into my life";
-		break;
-		case 'req_newtoarea':
-			$p_reqtype_text = "I recently moved to this area";
-		break;
-		case 'req_needspirithelp':
-			$p_reqtype_text = "I need spiritual help";
-		break;
-		case 'req_detailschange':
-			$p_reqtype_text = "I want to update my details";
-		break;
-		case 'req_newmember':
-			$p_reqtype_text = "I wish to become a new member";
-		break;
-		case 'req_newvolunteer':
-			$p_reqtype_text = "I wish to become a volunteer";
-		break;
-		default:
-			$p_reqtype_text = "";
-		break;
+		}
+		if(isset($_POST'req_newtoarea')){
+			$p_reqtype_text = "I am new to the area";
+		}
+		if(isset($_POST'req_needspirithelp')){
+			$p_reqtype_text = "I need spritirual help";
+		}
+		if(isset($_POST'req_detailschange')){
+			$p_reqtype_text = "My details have changed";
+		}
+		if(isset($_POST'req_newmember')){
+			$p_reqtype_text = "I want to become a new member";
+		}
+		if(isset($_POST'req_newvolunteer')){
+			$p_reqtype_text = "I want to become a volunteer";
+		}
 	}
 
 	$p_prayer = $_POST['prayer_req'];
